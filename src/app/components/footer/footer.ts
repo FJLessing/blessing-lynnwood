7bb06b5931ca0b1e5e2dba5c99be05b6d39018b7
FJ Lessing <me@fjlessing.co.za>
2025-11-23 20:53:33 +0200

    Add language switcher and about text arrays

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer {
+  private translate = inject(TranslateService);
+  currentLanguage = this.translate.currentLang || 'en';
 
+  switchLanguage(lang: string) {
+    this.translate.use(lang);
+    this.currentLanguage = lang;
+  }
 }
